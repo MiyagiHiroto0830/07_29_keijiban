3015f5f31ff9642775ead9bf4311efc85c6c0f34
MiyagiHiroto0830 <hirotobaseball0830@icloud.com>
2020-12-24 22:27:41 +0900

    first commit

--- kadai_table.sql
+++ kadai_table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: localhost
--- 生成日時: 2020 年 12 月 24 日 13:27
+-- 生成日時: 2020 年 12 月 24 日 14:26
 -- サーバのバージョン： 10.4.17-MariaDB
 -- PHP のバージョン: 7.3.24
 
@@ -24,10 +24,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- テーブルの構造 `kadai2_table`
+-- テーブルの構造 `kadai_table`
 --
 
-CREATE TABLE `kadai2_table` (
+CREATE TABLE `kadai_table` (
   `id` int(12) NOT NULL,
   `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -35,13 +35,30 @@ CREATE TABLE `kadai2_table` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- テーブルのデータのダンプ `kadai_table`
+--
+
+INSERT INTO `kadai_table` (`id`, `name`, `content`, `created_at`) VALUES
+(1, '宮城大翔', '2', '2020-12-23 14:14:29'),
+(2, '宮城大翔', '2', '2020-12-23 14:19:53'),
+(3, '宮城大翔', '2', '2020-12-23 15:38:47'),
+(4, 'アントニー', '2', '2020-12-23 16:14:23'),
+(5, 'アントニー', '2', '2020-12-23 16:23:42'),
+(6, '田中', '2', '2020-12-23 16:23:49'),
+(7, '宮城大翔', '2', '2020-12-23 16:59:14'),
+(8, 'hiroto', '2', '2020-12-23 17:10:23'),
+(9, '卍卍卍卍卍', '卍卍卍卍卍卍卍卍', '2020-12-23 17:25:09'),
+(10, '卍卍卍卍卍', '寒すぎ卍', '2020-12-23 17:25:24'),
+(11, '＠12', '助けてーーー', '2020-12-23 17:37:40');
+
+--
 -- ダンプしたテーブルのインデックス
 --
 
 --
--- テーブルのインデックス `kadai2_table`
+-- テーブルのインデックス `kadai_table`
 --
-ALTER TABLE `kadai2_table`
+ALTER TABLE `kadai_table`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -49,10 +66,10 @@ ALTER TABLE `kadai2_table`
 --
 
 --
--- テーブルの AUTO_INCREMENT `kadai2_table`
+-- テーブルの AUTO_INCREMENT `kadai_table`
 --
-ALTER TABLE `kadai2_table`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `kadai_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
